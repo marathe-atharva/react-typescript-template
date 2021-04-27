4c2c44f56159c188e9b26045f4fb592ba5f31e39
Atharva Marathe <atharvamarathe007@gmail.com>
2021-04-27 19:40:30 +0530

    Add huskey and lint-staged 🎉🎉

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,55 +1,55 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	entry: path.resolve(__dirname, "..", "./src/index.tsx"),
-	resolve: {
-		extensions: [".tsx", ".ts", ".jsx", ".js"],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|js)x?$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: "babel-loader",
-					},
-				],
-			},
-			{
-				test: /\.s[ac]ss$/,
-				use: [
-					"style-loader",
-					"css-loader",
-					{
-						loader: "sass-loader",
-						options: {
-							sourceMap: true,
-							sassOptions: {
-								outputStyle: "compressed",
-							},
-						},
-					},
-				],
-			},
-			{
-				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-				type: "asset/resource",
-			},
-			{
-				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-				type: "asset/inline",
-			},
-		],
-	},
-	output: {
-		path: path.resolve(__dirname, "..", "./build"),
-		filename: "bundle.js",
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, "..", "./src/index.html"),
-		}),
-	],
-};
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            sassOptions: {
+                                outputStyle: 'compressed',
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', './src/index.html'),
+        }),
+    ],
+}
