208072e7923c04d859efca103963ab68a34993de
Atharva Marathe <atharvamarathe007@gmail.com>
2021-04-28 01:26:01 +0530

    Add Google Analytics tag 🎉🎉

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { useEffect } from 'react'
 import './style.scss'
+import ReactGa from 'react-ga'
 
 export const App = () => {
+	useEffect(() => {
+		ReactGa.initialize(`${process.env.REACT_APP_GA}`)
+		ReactGa.pageview('/')
+	}, [])
+
 	return <h1 className="app">React Typescript Template! </h1>
 }
