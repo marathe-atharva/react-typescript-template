09df19e4bfad04f8efadec123c2d5f0cd2739fae
Atharva Marathe <atharvamarathe007@gmail.com>
2021-05-09 18:51:43 +0530

    Adding styled-components 🎉🎉

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,5 +1,18 @@
+import 'styled-components'
+
 declare module '*.png'
 declare module '*.svg' {
 	const content: string
 	export default content
+}
+
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		borderRadius: string
+
+		colors: {
+			main: string
+			secondary: string
+		}
+	}
 }
